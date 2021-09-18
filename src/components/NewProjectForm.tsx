59d6921639ae8696b7af3dad5c9a8ba0497d64e8
SamNal2008 <51299010+SamNal2008@users.noreply.github.com>
2021-09-18 14:04:25 +0200

    Update NewProjectForm.tsx

--- src/components/NewProjectForm.tsx
+++ src/components/NewProjectForm.tsx
@@ -83,7 +83,7 @@ const NewProjectForm = ({...props}: any) => {
             title: projectName,
             type: projectType,
             githubLink: githubLink,
-            content: '# Fill me',
+            content: '${projectDescription}',
             photoUrl: `${ContentType.projects}/${projectName}`
         }
         save(ContentType.projects, newProject);
